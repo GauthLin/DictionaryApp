6e6c70a0c39cb17ffc5d71160a2595f57e12878b
Guillaume Verfaille <Guillaume Verfaille>
2017-03-28 11:52:52 +0200

    Adding the preferences menu

--- app/src/main/java/be/ecam/dictionaryapp/MainActivity.java
+++ app/src/main/java/be/ecam/dictionaryapp/MainActivity.java
@@ -1,20 +1,55 @@
 package be.ecam.dictionaryapp;
 
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import be.ecam.dictionaryapp.Database.DictionaryDBHelper;
 import be.ecam.dictionaryapp.Database.WordContract;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements SharedPreferences.OnSharedPreferenceChangeListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+    }
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key){
+        if (key.equals(true)){
+            ChangeLanguage();
+        }
+    }
+
+    private void ChangeLanguage() {
+    }
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item){
+        int itemThatWasClickedId = item.getItemId();
+        if(itemThatWasClickedId == R.id.goToPreference){
+            Context context = MainActivity.this;
+            startActivity(new Intent(context, SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/be/ecam/dictionaryapp/SettingsActivity.java
+++ app/src/main/java/be/ecam/dictionaryapp/SettingsActivity.java
@@ -1,16 +1,32 @@
 package be.ecam.dictionaryapp;
 
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 /**
  * Created by Guillaume on 21-03-17.
  */
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView (R.layout.activity_settings );
+        setContentView(R.layout.activity_settings);
+        ActionBar actionBar = this.getSupportActionBar();
+        if(actionBar !=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int  id = item.getItemId();
+        if (id == android.R.id.home){
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
