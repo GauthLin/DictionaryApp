baba0416cefa1544f21075b6049bcf66647eaaab
Damien Vanhove <vanhovedamien@hotmail.com>
2017-03-28 09:34:12 +0200

    Add translation class, Edit Networking class

--- app/src/main/java/be/ecam/dictionaryapp/MainActivity.java
+++ app/src/main/java/be/ecam/dictionaryapp/MainActivity.java
@@ -1,7 +1,12 @@
 package be.ecam.dictionaryapp;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new AsyncNetworkingTask().execute();
+    }
+
+    public class AsyncNetworkingTask extends AsyncTask < String , Void , String > {
+        @Override
+        protected String doInBackground(String... params) {
+            String json = null;
+
+            try {
+                json = Networking.getApiTranslation();
+            }
+            catch (IOException error){
+                error.printStackTrace();
+            }
+
+            return json;
+        }
+
+        protected void onPostExecute(String queryResults) {
+
+            if (queryResults != null) {
+
+            } else {
+
+            }
+        }
     }
 }
