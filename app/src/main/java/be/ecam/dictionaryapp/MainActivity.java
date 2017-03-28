d729f43a50ccf646351bbc39e3c5b65f3118a183
Lancelot LHOEST <11163@ecam.be>
2017-03-28 11:03:44 +0200

    ajout de la NewWordActivity et communication entre celle-ci et la MainActivity

--- app/src/main/java/be/ecam/dictionaryapp/MainActivity.java
+++ app/src/main/java/be/ecam/dictionaryapp/MainActivity.java
@@ -1,11 +1,14 @@
 package be.ecam.dictionaryapp;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import be.ecam.dictionaryapp.Database.DictionaryDBHelper;
 import be.ecam.dictionaryapp.Database.WordContract;
@@ -16,5 +19,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NewWordActivity.class));
+            }
+        });
     }
+
+
 }
